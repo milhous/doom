53685de330534bf75e6532414eb4d23dd25e13e3
Milhous <milhous@sina.com>
2023-01-16 13:20:35 +0800

    A affiliate components

---
+++ packages/affiliate/app/components/home/HomeHow.tsx
@@ -0,0 +1,56 @@
+import { useTranslation } from '@app/libs/i18n/server';
+
+import Assets from '@app/assets';
+
+import './HomeHow.scss';
+
+// 如何加入
+const HomeHow = async (): Promise<JSX.Element> => {
+  const {t} = await useTranslation(['affiliate']);
+
+  return (
+    <section className="affiliate-how">
+      <h3>{t('how_title')}</h3>
+      <div className="how-list">
+        <p>{t('how_desc1')}</p>
+        <ul>
+          <li>
+            <div>
+              <Assets.HomeHowSN1 className="icon-sn" />
+              <Assets.HomeHowIcon1 className="how-icon1" />
+            </div>
+            <dl>
+              <dt>{t('how_subtitle1')}</dt>
+              <dd>{t('how_subdesc1')}</dd>
+            </dl>
+          </li>
+          <li>
+            <div>
+              <Assets.HomeHowSN2 className="icon-sn" />
+              <Assets.HomeHowIcon2 className="how-icon2" />
+            </div>
+            <dl>
+              <dt>{t('how_subtitle2')}</dt>
+              <dd>{t('how_subdesc2')}</dd>
+            </dl>
+          </li>
+          <li>
+            <div>
+              <Assets.HomeHowSN3 className="icon-sn" />
+              <Assets.HomeHowIcon3 className="how-icon3" />
+            </div>
+            <dl>
+              <dt>{t('how_subtitle3')}</dt>
+              <dd>{t('how_subdesc3')}</dd>
+            </dl>
+          </li>
+        </ul>
+      </div>
+      <a className="btn-apply" href="mailto:support@bitgame.com" title="support@bitgame.com">
+        {t('btn_apply')}
+      </a>
+    </section>
+  );
+};
+
+export default HomeHow;
--- packages/affiliate/app/page.tsx
+++ packages/affiliate/app/page.tsx
@@ -4,6 +4,12 @@ import Image from 'next/image';
 import Assets from './assets';
 
 import HomeBanner from '@app/components/home/HomeBanner';
+import HomePlan from '@app/components/home/HomePlan';
+import HomeCommission from '@app/components/home/HomeCommission';
+import HomeSupport from '@app/components/home/HomeSupport';
+import HomeRights from '@app/components/home/HomeRights';
+import HomeAdvantage from '@app/components/home/HomeAdvantage';
+import HomeHow from '@app/components/home/HomeHow';
 
 export default function Page() {
 
@@ -11,6 +17,18 @@ export default function Page() {
     <main className="affiliate affiliate-home">
       {/* @ts-expect-error Server Component */}
       <HomeBanner />
+      {/* @ts-expect-error Server Component */}
+      <HomePlan />
+      {/* @ts-expect-error Server Component */}
+      <HomeCommission />
+      {/* @ts-expect-error Server Component */}
+      <HomeSupport />
+      {/* @ts-expect-error Server Component */}
+      <HomeRights />
+      {/* @ts-expect-error Server Component */}
+      <HomeAdvantage />
+      {/* @ts-expect-error Server Component */}
+      <HomeHow />
       <h1 className="test">Hello, Next.js!!</h1>
       <h3 className="fontGradient">
         <span>Hello world!</span>
